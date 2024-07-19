08f11ef1a16eff6e8e207a13ec569a825d42ebb9
NarendraReddy <narendra.nya@gmail.com>
2024-07-18 18:41:58 -0700

    all

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -5,12 +5,9 @@
 static final String region(){
     return "us-east-1"
 }
-
 static final String nexusURL(){
     return "nexus.narendra.shop:8081"
 }
-
 static final String account_id(){
     return "905418111046"
 }
-
