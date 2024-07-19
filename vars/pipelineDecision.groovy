e0269163c7e49d1b07b4313aade41e24a0aa14e2
NarendraReddy <narendra.nya@gmail.com>
2024-07-18 18:41:11 -0700

    all

--- vars/pipelineDecision.groovy
+++ vars/pipelineDecision.groovy
@@ -1,18 +1,17 @@
 #!groovy
 
-// declaring a function
-def decidePipeline(Map configMap){
-   type = configMap.get("type")
-   switch(type) {
-    case "nodejsEKS":
-        nodejsEKS(configMap)        
-        break
-    case "nodejsVM":
-        nodejsVM(configMap)
-        break
-    default:
-        error "Type is not matching" 
-        break   
-   }
+//declaring a function
+def decidePipeline(Map configMap) {
+    type = configMap.get("type")
+    switch(type) {
+        case "nodejsEKS":
+            nodejsEKS(configMap)
+            break
+        case "nodejsVM":
+            nodejsVM(configMap)
+            break
+        default:
+            error "type is not matched" 
+            break
+    }
 }
-
