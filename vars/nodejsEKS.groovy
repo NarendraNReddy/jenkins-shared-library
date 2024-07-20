bbf04e3fc5fe4f29017fe01105abe0efb7d92825
NarendraReddy <narendra.nya@gmail.com>
2024-07-19 20:03:03 -0700

    all

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -60,7 +60,7 @@ def call(Map configMap){
                         aws eks update-kubeconfig --region ${region} --name ${project}-dev
                         cd helm
                         sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                        helm update ${component} -n ${project} .
+                        helm upgrade ${component} -n ${project} .
                     """
                 }
             }
