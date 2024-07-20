e0970466411b86f98d6c0f13d35b69910da16c32
NarendraReddy <narendra.nya@gmail.com>
2024-07-20 10:02:07 -0700

    ALL

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -55,7 +55,6 @@ def call(Map configMap){
                     """
                 }
             }
-
             stage('Deploy'){
                 steps{
                     // sh """
@@ -86,6 +85,19 @@ def call(Map configMap){
                         }
                     }                    
                     
+                }
+            }
+            stage('Verify Deployment'){
+                steps{
+                    script{
+                        rollbackStatus = sh(script: "kubectl rollout status deployment/backend --timeout=1m || true", returnStdout: true).trim()
+                        if(rollbackStatus.contains('successfully rolled out')){
+                            echo "Deployment is successfull"
+                        }
+                        else{
+                            echo "Deployment is failed, performing rollback"
+                        }
+                    }
                 }
             }
 
